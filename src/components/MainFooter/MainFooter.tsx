ce9f2283f7d729959d7912f659ee49b424feee28
Aleksandr Keizerov <78848997+AaleksanderK@users.noreply.github.com>
2023-08-09 10:10:36 +0300

    first commit

---
+++ src/components/MainFooter/MainFooter.tsx
@@ -0,0 +1,13 @@
+import "./MainFooter.css";
+const MainFooter = () => {
+  return (
+    <footer className="main-footer">
+      <div className="container">
+        <div className="main-footer__inner">
+          <p className="main-footer__copywriting">©2023 Blogfolio</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default MainFooter;
